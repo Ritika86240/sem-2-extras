f95d4815e0d3e2b1500e038cd17a9db7f7df28b4
RITIKA SHARMA <146440022+Ritika86240@users.noreply.github.com>
2024-03-10 20:13:35 +0530

    Add files via upload

---
+++ 7march.c
@@ -0,0 +1,6 @@
+// given alist the length has to be doubled and the last element has to same as that of the 
+//original elements the rest of the elements to be displayed as zeros
+
+
+#include<stdio.h>
+#include<stdlib.h>
